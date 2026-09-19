fbfc6ff1c0fcfd883a79bdb8ec838a25456524f8
Wesley C.C. Barnett <W3513yb4rn3tt@gmail.com>
2026-09-19 17:01:09 -0600

    Refine Q-OS causal wavefront law: qos/include/qos/qkernel.h

--- qos/include/qos/qkernel.h
+++ qos/include/qos/qkernel.h
@@ -15,9 +15,9 @@
 #endif
 
 typedef enum {
-    Q_EDGE_CASCADE = 0,
-    Q_EDGE_DEEPEN  = 1,
-    Q_EDGE_CANCEL  = 2
+    Q_EDGE_CASCADE = 0, /* transmit same strike once */
+    Q_EDGE_DEEPEN  = 1, /* transmit same strike twice */
+    Q_EDGE_CANCEL  = 2  /* transmit opposite strike once */
 } qedge_mode_t;
 
 typedef struct {
@@ -33,15 +33,20 @@ typedef struct {
 } qevent_t;
 
 typedef struct {
-    uint8_t state[(QOS_MAX_NODES + 3u) / 4u];
-    uint8_t allocated[(QOS_MAX_NODES + 7u) / 8u];
+    uint8_t state[(QOS_MAX_NODES + 3u) / 4u]; /* 2 bits per allocated node */
+    uint8_t allocated[(QOS_MAX_NODES + 7u) / 8u]; /* Null is absence, not state */
     qedge_t edges[QOS_MAX_EDGES];
     uint16_t edge_count;
     qevent_t queue[QOS_MAX_EVENTS];
     uint16_t qhead;
     uint16_t qtail;
+    /* Fixed sparse wavefront scratch. This is runtime workspace, not node state. */
+    int16_t pending[QOS_MAX_NODES];
+    uint16_t touched[QOS_MAX_NODES];
+    uint8_t pending_mark[(QOS_MAX_NODES + 7u) / 8u];
     uint32_t transitions;
     uint32_t events_processed;
+    uint32_t wavefronts_processed;
 } qkernel_t;
 
 void qk_init(qkernel_t *k);
