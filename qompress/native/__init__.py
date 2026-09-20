9380b6299f95bfbb27eba25955fde98daa48d2a0
Wesley C.C. Barnett <W3513yb4rn3tt@gmail.com>
2026-09-20 04:17:44 -0600

    Package native Q-register bridge

---
+++ qompress/native/__init__.py
@@ -0,0 +1,3 @@
+from .native_register import NativeFieldState, integer_to_qstates, qstates_to_integer, zigzag_decode, zigzag_encode
+
+__all__ = ["NativeFieldState", "integer_to_qstates", "qstates_to_integer", "zigzag_decode", "zigzag_encode"]
